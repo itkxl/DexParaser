0db6f209d9fa9bce11a7971d7d726f103665b1ed
itkxl <32793228+itkxl@users.noreply.github.com>
2020-09-17 00:13:35 +0800

    dex头，魔数的学习

---
+++ src/com/itkxl/dex/parser/impl/StringIdItemParser.java
@@ -0,0 +1,14 @@
+package com.itkxl.dex.parser.impl;
+
+import com.itkxl.dex.parser.AbsParser;
+
+public class StringIdItemParser extends AbsParser {
+
+
+
+
+    @Override
+    public void parse(byte[] bytes) {
+
+    }
+}
